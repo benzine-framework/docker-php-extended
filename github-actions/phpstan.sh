4ca3286fa5d96677f64b82b76d30b4cb0464ddae
Matthew Baggett <matthew@baggett.me>
2020-11-27 15:48:31 +0100

    Come on, build for me?

---
+++ github-actions/phpstan.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+/composer/vendor/bin/phpstan --version
+
+echo "::group::Installed PHPStan extensions"
+composer show "*phpstan*"
+echo "::endgroup::"
+
+IGNORE_PLATFORM_REQS=""
+if [ "$CHECK_PLATFORM_REQUIREMENTS" = "false" ]; then
+    IGNORE_PLATFORM_REQS="--ignore-platform-reqs"
+fi
+
+NO_DEV="--no-dev"
+if [ "$REQUIRE_DEV" = "true" ]; then
+    NO_DEV=""
+fi
+
+COMPOSER_COMMAND="composer install --no-scripts --no-progress $NO_DEV $IGNORE_PLATFORM_REQS"
+echo "::group::$COMPOSER_COMMAND"
+$COMPOSER_COMMAND
+echo "::endgroup::"
+/composer/vendor/bin/phpstan $*
